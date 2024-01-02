0eb663f1a715b2e3789a090043ef579da9fdd12a
karthickraja2303 <karthickraja.n@theswipewire.in>
2024-01-02 21:12:21 +0530

    App Router Study

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -24,7 +24,7 @@ const Links = () => {
       <div> 
             {
                   links.map((link=>(
-                        <Link href="{link.path}" key="{link.title}">{link.title}</Link>
+                        <Link href={link.path} key={link.title} >{link.title}</Link>
                   )))
             }
       </div>);
