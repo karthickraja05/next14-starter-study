254c141d82271da1e427de7b102be79f81722b30
Karthickraja <karthickraja.gm@gmail.com>
2024-01-12 11:29:11 +0530

    about us page styling

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,12 +2,33 @@ import Image from "next/image";
 import styles from "./about.module.css";
 
 const AboutPage = () => {
-      return (<div className={styles.container}>
-                  <div className={styles.imgContainer}>
-                        <Image src={'/img/about.png'} alt="About us Images" fill />
-                        {/* <Image src={'https://images.pexels.com/photos/19594011/pexels-photo-19594011/free-photo-of-woman-crouching-on-a-street.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} alt="About us Images" fill /> */}
+      return (
+            <div className={styles.container}>
+                  <div className={styles.textContainer}>
+                        <div className={styles.title}>About Agency</div>
+                        <div className={styles.title2}>We create digital ideas that are bigger, bolder, braver and better.</div>
+                        <div className={styles.description}>we believe in the transformative power of digital marketing to elevate brands and drive business success. Established with a passion for innovation and a commitment to delivering results, we have emerged as a leading force in the dynamic landscape of digital marketing.</div>
+                        <div className={styles.data}>
+                              <div className={styles.statistics}>
+                                    <div className={styles.count}>10 K+</div>
+                                    <div className={styles.count_text}>Year of experience</div>
+                              </div>
+                              <div className={styles.statistics}>
+                                    <div className={styles.count}>234 K+</div>
+                                    <div className={styles.count_text}>People reached</div>
+                              </div>
+                              <div className={styles.statistics}>
+                                    <div className={styles.count}>5 K+</div>
+                                    <div className={styles.count_text}>Services and plugins</div>
+                              </div>
+                        </div>
                   </div>
-            </div>);
+                  <div className={styles.imgContainer}>
+                        <Image src={'/img/about.png'} alt="About us Images" fill className={styles.img}
+                        />
+                  </div>
+            </div>
+      );
 };
 
 export default AboutPage;
