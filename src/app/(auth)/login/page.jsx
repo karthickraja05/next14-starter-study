6c6cac5b763b314a49f0db456241023af5e6b44b
Karthickraja <karthickraja.gm@gmail.com>
2024-01-26 09:40:42 +0530

    login page design

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,13 +1,26 @@
 import { handleGithubLogin } from "@/lib/action";
+import styles from './login.module.css';
+
 
 const Login = async () => {
 
 
-      return <div>
-            <form action={handleGithubLogin}>
-                  <button>Login with GitHub</button>
-            </form>
-      </div>;
+      return (
+            <div className={styles.container}>
+                  <div className={styles.github}>
+                        <form action={handleGithubLogin}>
+                              <button>Login with GitHub</button>
+                        </form>
+                  </div>
+                  <div className={styles.login_creden}>
+                        <form action={handleGithubLogin}>
+                              <input type="text" name="username" placeholder="UserName"/>
+                              <input type="password" name="password" placeholder="Password"/>
+                              <button>Login with Credentials</button>
+                        </form>
+                  </div>
+            </div>
+      );
 };
 
 export default Login;
